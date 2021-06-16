0aaca7297fc84b2884265ec7a511c472c91fd549
Franco <69213724+francoec@users.noreply.github.com>
2021-06-16 11:04:58 -0300

    Update CP002.js

--- CP002.js
+++ CP002.js
@@ -7,6 +7,6 @@ describe('CP002-Aplicar filtro de equipos -Gama.Alta -Memoria Interna.256GB', fu
         cy.get(':nth-child(4) > .filter-group > .filter-data-section > :nth-child(3) > a > .filter-info-label').click(); //click en filtro "Gama Alta"
         cy.get('li > .btn-group > .btn-filter').click(); //click nuevamente en 'Filtrar por'
         cy.get(':nth-child(3) > .filter-group > .filter-data-section > :nth-child(2) > a > .filter-info-label').click(); //click en filtro "256GB"
-        cy.get('.toolbar-bottom > .toolbar > .pager > .count-container > .amount > strong').should('contain','Artículo(s)'); // Se checkea que el artículo tenga 12 cuotas
+        cy.get('.toolbar-bottom > .toolbar > .pager > .count-container > .amount > strong').should('contain','Artículo(s)'); // Se checkea la cantidad de artículos devuelve la busqueda.
     }) 
 })
